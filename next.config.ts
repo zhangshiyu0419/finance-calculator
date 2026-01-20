0fb18e1e7da27fceb13e3d69bb49844abc8402c1
zhangshiyu0419 <zhangshiyu0419@gmail.com>
2026-01-20 22:24:47 +0800

    fix: 修正文件路径配置

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,7 @@ import type { NextConfig } from 'next';
 import path from 'path';
 
 const nextConfig: NextConfig = {
-  outputFileTracingRoot: path.resolve(__dirname, '../../'),
-  /* config options here */
+  outputFileTracingRoot: path.resolve(__dirname),
   allowedDevOrigins: ['*.dev.coze.site'],
   images: {
     remotePatterns: [
